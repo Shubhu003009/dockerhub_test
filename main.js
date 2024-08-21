508585a3b91a302813330b0430726167b807d13a
shubhu003009 <shubhamjangid990@gmail.com>
2024-08-21 13:45:38 +0530

    testing rollout in kubernetes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hey, node inside container");
-  console.log("Hey, node inside container");
+  console.log("hello from NODE.js application running inside DOCKER CONTAINER using node:current-apline3.19 image which is running inside KUBERNETES POD and DEPLOYEMNT dockerhub-deploy");
 });
 
 app.listen(PORT, () => {
