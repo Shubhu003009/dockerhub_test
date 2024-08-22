0f4567cd409e8679eda7607b76a723c93655ecd5
Shubhu003009 <110506200+Shubhu003009@users.noreply.github.com>
2024-08-22 13:55:15 +0530

    made some error for rollout checking

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+app.gt("/", (req, res) => {
   res.send("Hey, node inside container");
   console.log("hello from NODE.js application running inside DOCKER CONTAINER using node:current-apline3.19 image which is running inside KUBERNETES POD and DEPLOYEMNT dockerhub-deploy");
 });
 
-app.listen(PORT, () => {
+app.lisen(PORT, () => {
   console.log(`server started on http://localhost:${PORT}`);
 });
