d169eacbd54d4b3dfad168b6015c393d36d76665
Shubhu003009 <110506200+Shubhu003009@users.noreply.github.com>
2024-08-22 15:23:03 +0530

    fixed

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.gt("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hey, node inside container");
   console.log("hello from NODE.js application running inside DOCKER CONTAINER using node:current-apline3.19 image which is running inside KUBERNETES POD and DEPLOYEMNT dockerhub-deploy");
 });
 
-app.lisen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server started on http://localhost:${PORT}`);
 });
